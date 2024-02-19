e901fb562e71ccaa4158d3e108851c6fd8e35b80
LiranBashari <liranbashari1@gmail.com>
2024-02-19 20:44:44 +0200

    css the scrollbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,9 +6,9 @@ import { faGraduationCap, faBriefcase, faUser } from '@fortawesome/free-solid-sv
 
 const Home = () => {
   return (
-    <div style={{ height: '100vh', width: '100vw'}}>
+    <div style={{ height: '100vh', width: '100vw', overflowX: 'hidden' }}>
       <Navbar backgroundColor="bg-dark" />
-      <div className="container-fluid" style={{ overflowX: 'auto', whiteSpace: 'nowrap' }}>
+      <div className="container-fluid">
         <div className="row">
           <div className="col-md-2 p-4">
             <img src={profile} alt="profile" className="img-fluid rounded-circle" />
