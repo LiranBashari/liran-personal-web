9b1a4c0718b71c5b74cd9aff937fd040c7f0f65e
LiranBashari <liranbashari1@gmail.com>
2024-02-19 23:48:48 +0200

    fix toggle issue (not opened)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,30 +6,29 @@ const Navbar = ({ backgroundColor }) => {
     <nav className={`navbar navbar-expand-lg navbar-dark ${backgroundColor}`}>
       <div className="container-fluid">
         <div className="navbar-brand">Liran Bashari</div>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
+        <button 
+          className="navbar-toggler collapsed" 
+          type="button" 
+          data-bs-toggle="collapse" 
+          data-bs-target="#navbar" 
+          aria-controls="navbar" 
+          aria-expanded="false" 
+          aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+        <div className="navbar-collapse collapse justify-content-end" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <NavLink className="nav-link pe-lg-4" activeClassName="active" to="/">Home</NavLink>
+              <NavLink className="nav-link pe-lg-4" activeclassname="active" to="/">Home</NavLink>
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link pe-lg-4" activeClassName="active" to="/portfolio">Portfolio</NavLink>
+              <NavLink className="nav-link pe-lg-4" activeclassname="active" to="/portfolio">Portfolio</NavLink>
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link pe-lg-4" activeClassName="active" to="/resume">Resume</NavLink>
+              <NavLink className="nav-link pe-lg-4" activeclassname="active" to="/resume">Resume</NavLink>
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link pe-lg-4" activeClassName="active" to="/contact">Contact</NavLink>
+              <NavLink className="nav-link pe-lg-4" activeclassname="active" to="/contact">Contact</NavLink>
             </li>
           </ul>
         </div>
