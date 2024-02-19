4950e555f68520eb89262923b82fa68e2f1f0add
LiranBashari <liranbashari1@gmail.com>
2024-02-19 21:03:29 +0200

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -9,38 +9,38 @@ const Contact = () => {
     <div>
       <Navbar backgroundColor="bg-dark"/>
       <div className="container">
-        <h1 className='text-white pt-5 display-2'>
+        <h1 className='text-white pt-5 display-1 text-center' style={{ marginLeft: '-20vh' }}>
           <FontAwesomeIcon icon={faEnvelope} className="mr-3"/> Contact Me
         </h1>
         <br/>
         <div className="row mt-5">
-          <div className="col-md-6 pt-5 pr-md-4">
-            <h3 className="text-white display-6">
-              <FontAwesomeIcon icon={faPhoneAlt} className="mr-3"/> Phone Number
+          <div className="col-md-4 offset-md-2 pt-3 pr-md-4">
+            <h3 className="text-white display-5">
+              <FontAwesomeIcon icon={faPhoneAlt} className="mr-3"/> Phone
             </h3>
           </div>
-          <div className="col-md-6 pt-5">
-            <p className="text-white display-6">053-6204838</p>
+          <div className="col-md-6 pt-3">
+            <p className="text-white display-5">053-6204838</p>
           </div>
         </div>
         <div className="row mt-5">
-          <div className="col-md-6 pt-5 pr-md-4">
-            <h3 className="text-white display-6">
+          <div className="col-md-4 offset-md-2 pt-3 pr-md-4">
+            <h3 className="text-white display-5">
               <FontAwesomeIcon icon={faGithubSquare} className="mr-3"/> GitHub
             </h3>
           </div>
-          <div className="col-md-6 pt-5">
-            <a href="https://github.com/LiranBashari" className="text-white display-6" style={{ display: 'inline-block', whiteSpace: 'nowrap' }}>https://github.com/LiranBashari</a>
+          <div className="col-md-6 pt-3">
+            <a href="https://github.com/LiranBashari" className="text-white display-5" style={{ display: 'inline-block', whiteSpace: 'nowrap' }}>My GitHub</a>
           </div>
         </div>
         <div className="row mt-5">
-          <div className="col-md-6 pt-5 pr-md-4">
-            <h3 className="text-white display-6">
+          <div className="col-md-4 offset-md-2 pt-3 pr-md-4">
+            <h3 className="text-white display-5">
               <FontAwesomeIcon icon={faLinkedin} className="mr-3"/> LinkedIn
             </h3>
           </div>
-          <div className="col-md-6 pt-5">
-            <a href="https://www.linkedin.com/in/liran-bashari-3a0129216/" className="text-white display-6" style={{ display: 'inline-block', whiteSpace: 'nowrap' }}>https://www.linkedin.com/in/liran-bashari</a>
+          <div className="col-md-6 pt-3">
+            <a href="https://www.linkedin.com/in/liran-bashari-3a0129216/" className="text-white display-5" style={{ display: 'inline-block', whiteSpace: 'nowrap' }}>My LinkedIn</a>
           </div>
         </div>
       </div>
